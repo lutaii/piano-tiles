0753691893949be2558cc1b1a6a59101c02eab12
lutaii <ivanlutaienko@gmail.com>
2024-04-11 23:21:49 +0200

    add colors palette

---
+++ utils/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const defaultColor = Color(0xFFDCDBD7);
+  static const errorColor = Color(0xFFAA453A);
+  static const enabledColor = Color(0xFF404763);
+  static const tappedColor = Color(0xFF3A9F6C);
+  static const scoreColor = Color(0xFF1E3727);
+}
